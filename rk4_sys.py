ddcc1573d71c4b2046cfd463da7e0fa9571e828b
ucfilho <42356381+ucfilho@users.noreply.github.com>
2021-05-28 16:02:47 -0300

    Update rk4_sys.py

--- rk4_sys.py
+++ rk4_sys.py
@@ -1,20 +1,22 @@
 import numpy as np
 
-def rk4( f, t0, tf,x0, h =0.001): 
-
+def rk4( f, t0, tf,x0, h =0.001):
     n = int((tf -t0)/h + 1)
     nx = len(x0)
     x = np.array( [ x0 ] * n )
     t = np.linspace( t0, tf, n )
-    k1 = np.zeros(nx, dtype = float) 
-    k2 = np.zeros(nx, dtype = float) 
-    k3 = np.zeros(nx, dtype = float) 
-    k4 = np.zeros(nx, dtype = float) 
-    xnew = np.zeros(nx, dtype = float)
-    print(xnew)
-    print(k1)
+    if(nx > 1):
+        k1 = np.zeros(nx, dtype = float) 
+        k2 = np.zeros(nx, dtype = float) 
+        k3 = np.zeros(nx, dtype = float) 
+        k4 = np.zeros(nx, dtype = float) 
+        xnew = np.zeros(nx, dtype = float)
+     else:
+        k1=[[0]];k2=[[0]];k3=[[0]];k4=[[0]];xnew = [[0]]
+     print(xnew)
+     print(k1)
 
-    for i in range( n - 1 ):
+     for i in range( n - 1 ):
         xnew =[x[i,j] for j in range(nx)]
         k1 = np.multiply(h ,f( xnew, t[i] ))
         xnew =[x[i,j]+0.5*k1[j] for j in range(nx)]
@@ -23,8 +25,8 @@ def rk4( f, t0, tf,x0, h =0.001):
         k3 = np.multiply(h ,f( xnew, t[i] + 0.5 * h ))
         xnew =[x[i,j]+ k3[j] for j in range(nx)]
         k4 = np.multiply(h ,f(xnew, t[i+1] ))
-      
-        for j in range(nx):
-            x[i+1,j] = x[i,j] + ( k1[j] + 2.0 * ( k2[j]  + k3[j]  ) + k4[j]  ) / 6.0
-    
-    return t,x
+
+     for j in range(nx):
+        x[i+1,j] = x[i,j] + ( k1[j] + 2.0 * ( k2[j]  + k3[j]  ) + k4[j]  ) / 6.0
+
+     return t,x
