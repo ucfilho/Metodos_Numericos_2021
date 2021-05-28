ca48d84d0279912adb570c237997c2221867ebc3
ucfilho <42356381+ucfilho@users.noreply.github.com>
2021-05-28 16:04:28 -0300

    Update rk4_sys.py

--- rk4_sys.py
+++ rk4_sys.py
@@ -11,12 +11,12 @@ def rk4( f, t0, tf,x0, h =0.001):
         k3 = np.zeros(nx, dtype = float) 
         k4 = np.zeros(nx, dtype = float) 
         xnew = np.zeros(nx, dtype = float)
-     else:
+    else:
         k1=[[0]];k2=[[0]];k3=[[0]];k4=[[0]];xnew = [[0]]
-     print(xnew)
-     print(k1)
+    print(xnew)
+    print(k1)
 
-     for i in range( n - 1 ):
+    for i in range( n - 1 ):
         xnew =[x[i,j] for j in range(nx)]
         k1 = np.multiply(h ,f( xnew, t[i] ))
         xnew =[x[i,j]+0.5*k1[j] for j in range(nx)]
@@ -26,7 +26,7 @@ def rk4( f, t0, tf,x0, h =0.001):
         xnew =[x[i,j]+ k3[j] for j in range(nx)]
         k4 = np.multiply(h ,f(xnew, t[i+1] ))
 
-     for j in range(nx):
+    for j in range(nx):
         x[i+1,j] = x[i,j] + ( k1[j] + 2.0 * ( k2[j]  + k3[j]  ) + k4[j]  ) / 6.0
 
-     return t,x
+    return t,x
